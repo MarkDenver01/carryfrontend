9db2ad3e1aa08dbeb2c5d2ef4cc784f3c4131c8c
Denver <gregoriomarkdenver01@gmail.com>
2025-11-06 02:16:28 +0800

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ['carryfrontend.onrender.com'],
+    allowedHosts: ['capstone.wrapandcarry.com'],
   },
 })
