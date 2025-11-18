a191cc5ab5d2fc65b094a2b74e59b3aa371aa0e7
Denver <gregoriomarkdenver01@gmail.com>
2025-11-18 18:24:35 +0800

    never used

--- src/layout/product/ProductInventoryTable.tsx
+++ src/layout/product/ProductInventoryTable.tsx
@@ -35,7 +35,6 @@ export default function ProductInventoryTable() {
   // ✅ Modal state for viewing recommendations
   const [viewModal, setViewModal] = useState(false);
   const [recommendations, setRecommendations] = useState<RecommendationRuleDTO[]>([]);
-  const [ setSelectedProductId] = useState<number | null>(null);
 
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 8;
