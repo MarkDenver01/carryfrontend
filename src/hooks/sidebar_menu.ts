38dfefc6097d0846565384e4c3af5f555c7f2c3f
Tani <gregorionathaniel01@gmail.com>
2025-11-22 03:34:00 +0800

    update sidebar_menu

--- src/hooks/sidebar_menu.ts
+++ src/hooks/sidebar_menu.ts
@@ -12,106 +12,117 @@
     } from "lucide-react";
     import type { SidebarMenuSection } from "../types/sidebar.ts";
 
-    export const sidebarMenu: SidebarMenuSection[] = [
-        {
-            section: "Main",
-            items: [
-                {
-                    label: "Dashboard",
-                    icon: LayoutDashboard,
-                    path: "/dashboard",
-                },
-            ],
-        },
-        {
-            section: "Product",
-            items: [
-                {
-                    label: "Product Management",
-                    icon: Boxes,
-                    children: [
-                        {
-                            label: "Product Categories",
-                            icon: Boxes,    // or any icon you used
-                            path: "/dashboard/products/categories",
-                        },
-                        {
-                            label: "Product Monitoring",
-                            icon: Search,
-                            path: "/dashboard/products/monitoring",
-                        },
-                        {
-                            label: "Product Recommendation",
-                            icon: Star,
-                            path: "/dashboard/products/recommendation",
-                        },
-                        {
-                            label: "Product Rates",
-                            icon: Currency,
-                            path: "/dashboard/products/rates",
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            section: "Analytics",
-            items: [
-                {
-                    label: "Analytics Dashboard",
-                    icon: BarChart2,
-                    children: [
-                        {
-                            label: "Customer Report",
-                            icon: Users,
-                            path: "/dashboard/analytics/customers",
-                            role: "ADMIN"
-                        },
-                        {
-                            label: "Sales Report",
-                            icon: ShoppingCart,
-                            path: "/dashboard/analytics/sales",
-                            role: "ADMIN"
-                        },
-                        {
-                            label: "Product Report",
-                            icon: Boxes,
-                            path: "/dashboard/analytics/products",
-                            role: "SUB_ADMIN"
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            section: "Operations",
-            items: [
-                {
-                    label: "Customer Membership",
-                    icon: UserCheck,
-                    path: "/dashboard/users",
-                },
-                {
-                    label: "Delivery Management",
-                    icon: Truck,
-                    children: [
-                        {
-                            label: "Orders",
-                            icon: ShoppingCart,
-                            path: "/dashboard/delivery/orders",
-                        },
-                        {
-                            label: "Available Driver",
-                            icon: Truck,
-                            path: "/dashboard/delivery/riders",
-                        },
-                        {
-                        label: "Add Driver",
-                        icon: Truck,
-                        path: "/dashboard/delivery/add-rider",
+export const sidebarMenu: SidebarMenuSection[] = [
+    {
+        section: "Main",
+        items: [
+            {
+                label: "Dashboard",
+                icon: LayoutDashboard,
+                path: "/dashboard",
+            },
+        ],
+    },
+
+    {
+        section: "Product",
+        items: [
+            {
+                label: "Product Management",
+                icon: Boxes,
+                children: [
+                    {
+                        label: "Product Categories",
+                        icon: Boxes,
+                        path: "/dashboard/products/categories",
+                        role: "SUB_ADMIN",
+                    },
+                    {
+                        label: "Product Monitoring",
+                        icon: Search,
+                        path: "/dashboard/products/monitoring",
+                        role: "SUB_ADMIN",
+                    },
+                    {
+                        label: "Product Recommendation",
+                        icon: Star,
+                        path: "/dashboard/products/recommendation",
+                        role: "SUB_ADMIN",
+                    },
+                    {
+                        label: "Product Rates",
+                        icon: Currency,
+                        path: "/dashboard/products/rates",
+                        role: "SUB_ADMIN",
                     },
                 ],
-                },
-            ],
-        },
-    ];
+            },
+        ],
+    },
+
+    {
+        section: "Analytics",
+        items: [
+            {
+                label: "Analytics Dashboard",
+                icon: BarChart2,
+                children: [
+                    {
+                        label: "Customer Report",
+                        icon: Users,
+                        path: "/dashboard/analytics/customers",
+                        role: "ADMIN",
+                    },
+                    {
+                        label: "Sales Report",
+                        icon: ShoppingCart,
+                        path: "/dashboard/analytics/sales",
+                        role: "ADMIN",
+                    },
+                    {
+                        label: "Product Report",
+                        icon: Boxes,
+                        path: "/dashboard/analytics/products",
+                        role: "SUB_ADMIN",
+                    },
+                ],
+            },
+        ],
+    },
+
+    {
+        section: "Operations",
+        items: [
+            {
+                label: "Customer Membership",
+                icon: UserCheck,
+                path: "/dashboard/users",
+                role: "SUB_ADMIN",
+            },
+            {
+                label: "Delivery Management",
+                icon: Truck,
+                children: [
+                    {
+                        label: "Orders",
+                        icon: ShoppingCart,
+                        path: "/dashboard/delivery/orders",
+                        role: "SUB_ADMIN",
+                    },
+                    {
+                        label: "Available Driver",
+                        icon: Truck,
+                        path: "/dashboard/delivery/riders",
+                        role: "SUB_ADMIN",
+                    },
+                    {
+                        label: "Add Driver",
+                        icon: Truck,
+                        path: "/dashboard/delivery/add-riders",
+                        role: "SUB_ADMIN",
+                    },
+                ],
+            },
+        ],
+    },
+];
