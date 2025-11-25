35d2a53cacf00dcd2b629e04b41b4ff98aad3c32
Denver <gregoriomarkdenver01@gmail.com>
2025-11-26 02:14:49 +0800

    order

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import React from "react";
 
-// CONTEXT PROVIDERS
 import { AuthProvider } from "./context/AuthContext";
-import { DriverProvider } from "./context/DriverContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <DriverProvider>
-          <App />
-        </DriverProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
